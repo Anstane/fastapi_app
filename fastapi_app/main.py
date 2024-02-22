9e3faf84167f97a73e0fe1f73eeef8cdfba9a7f9
Mikhail Moskovkin <anstane228@gmail.com>
2024-02-23 01:10:47 +0300

    New feature and fixes

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -9,11 +9,11 @@ from data import router as router_v1
 async def lifespan(app: FastAPI):
     """
     События цикла жизни приложения.
-    При запуске создаются все очищается старая и создаётся новая таблица.
+    При запуске очищается старая и создаётся новая таблица.
     """
 
     async with db_help.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all) # Закомментировать если не хотим удалять старую таблицу при запуске.
         await conn.run_sync(Base.metadata.create_all)
     yield
 
